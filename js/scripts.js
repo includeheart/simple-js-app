706f76c2dfb717356dc1cc81cbc2659563b2da20
Patrick Kernan <pkernan18@yahoo.com>
2024-08-07 14:03:02 -0400

    Switched to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,14 @@ let pokemonList = [
     {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
+/* for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.5) {
         document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - That’s a big boy<br>'); //This will check if the height is greater than 1.5, and if it is, it will print "That’s a big boy!" after the height.
     } else {
         document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')<br>'); //This will print the name and height of the Pokémon if the height is not greater than 1.5. At this point, it should only show Venosaur with the comment.
     }
-}
+}*/
+
+pokemonList.forEach(function(pokemon) {
+    document.write(pokemon.name + ' is ' + pokemon.height + 'm tall, and ' + pokemon.types + ' type ' + '<br>'); //I couldn't figure out how to separate the 'Grass' and 'Poison' types, either with a '/' or something else.
+})
