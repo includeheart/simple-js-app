3acdd8c6f455548021b463ee31bf6892b4545a5d
Patrick Kernan <pkernan18@yahoo.com>
2024-08-12 12:59:16 -0400

    Adjusted indentations

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-        {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-        {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
-    ];
-    function add(pokemon) {
-        pokemonList.push(pokemon);
-      }
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
+    {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
+   ];
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
     
-    function getAll() {
-        return pokemonList;
-      }
+  function getAll() {
+      return pokemonList;
+    }
     
-    return {
-        add: add,
-        getAll: getAll
-      };
+  return {
+      add: add,
+      getAll: getAll
+    };
 })();
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-    document.write(pokemon.name + ' is ' + pokemon.height + 'm tall and ' + pokemon.types + ' type ' + '<br>'); //Haven't figured out how to separate the two types
+  document.write(pokemon.name + ' is ' + pokemon.height + 'm tall and ' + pokemon.types + ' type ' + '<br>'); //Haven't figured out how to separate the two types
 });
