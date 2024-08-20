c39ae2628bcf487c77e1840e6e4d83473aa3431f
Patrick Kernan <pkernan18@yahoo.com>
2024-08-20 14:48:21 -0400

    Full update to the javascript, making them buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,52 @@ let pokemonRepository = (function () {
     {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
     {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
    ];
+
   function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
       pokemonList.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
     }
+  }
     
   function getAll() {
       return pokemonList;
     }
-    
+
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.classList.add('button-class');
+    button.innerText = pokemon.name;
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    button.addEventListener('click', function(){
+      showDetails(pokemon);
+    });
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
+
   return {
       add: add,
-      getAll: getAll
-    };
+      getAll: getAll,
+      addListItem: addListItem
+  };
 })();
 
+pokemonRepository.add({ name: 'Charmander', height: 0.6, types: ['fire'] });
+
+console.log(pokemonRepository.getAll());
+
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(pokemon.name + ' is ' + pokemon.height + 'm tall and ' + pokemon.types + ' type ' + '<br>'); //Haven't figured out how to separate the two types
+  pokemonRepository.addListItem(pokemon);
 });
