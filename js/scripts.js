6cac9346f5dd481873a4a59608816cb3c2ee78de
Patrick Kernan <pkernan18@yahoo.com>
2024-09-06 10:27:54 -0400

    Added pokedex API and polyfills

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Ivysaur', height: 1, types: ['grass', 'poison']},
-    {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
-   ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function add(pokemon) {
     if (
       typeof pokemon === "object" &&
-      "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
+      "name" in pokemon
     ) {
       pokemonList.push(pokemon);
     } else {
@@ -19,37 +14,70 @@ let pokemonRepository = (function () {
   }
     
   function getAll() {
-      return pokemonList;
-    }
+    return pokemonList;
+  }
 
   function addListItem(pokemon){
-    let pokemonList = document.querySelector('.pokemon-list');
-    let listItem = document.createElement('li');
-    let button = document.createElement('button');
-    button.classList.add('button-class');
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.classList.add("button-class");
     button.innerText = pokemon.name;
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
-    button.addEventListener('click', function(){
+    button.addEventListener("click", function(event) {
       showDetails(pokemon);
     });
   }
+  
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  } 
 
   function showDetails(pokemon){
-    console.log(pokemon);
+    pokemonRepository.loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
 
   return {
-      add: add,
-      getAll: getAll,
-      addListItem: addListItem
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
 })();
 
-pokemonRepository.add({ name: 'Charmander', height: 0.6, types: ['fire'] });
-
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
